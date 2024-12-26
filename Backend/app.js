d5d06880f9d3e5844526096445195bbcff93d715
Deepak  HP <deepakhp19@gmail.com>
2024-12-27 01:36:43 +0530

    Adding mongoDb

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+const connectToDB = require("./db/db")
+connectToDB();
 app.use(cors())
 
 app.get("/", (req,res) => {
