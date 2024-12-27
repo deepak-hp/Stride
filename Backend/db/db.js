cb6da1b242138eaf4e5d2b38c745b12af1ee3fe0
Deepak  HP <deepakhp19@gmail.com>
2024-12-27 19:41:37 +0530

    removed depicated options

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDB() {
-    mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology:true}).then(()=> {
+    mongoose.connect(process.env.DB_CONNECT).then(()=> {
         console.log("Connected to DB")
     }).catch(err => console.log(err))
 }
