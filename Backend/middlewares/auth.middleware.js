4b4d7f7ec98d150b42c1158d2eef658129587e26
Deepak  HP <deepakhp19@gmail.com>
2024-12-29 01:56:38 +0530

    removed logs

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -33,19 +33,15 @@ module.exports.authUser = async (req, res, next) => {
 module.exports.authCaptain = async (req, res, next) => {
     try {
         const token = req.cookies.token || req.headers.authorization.split(" ")[1];
-       
         if (!token) {
             return res.status(401).json({ message: "Unauthorized" })
         }
 
         const isBlacklistedToken = await blacklistTokenModel.findOne({ token })
-        console.log(isBlacklistedToken)
-
         if (isBlacklistedToken) {
             return res.status(401).json({ message: "Unauthorized" })
         }
 
-        
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const captain = await captainModel.findById(decoded._id);
 
