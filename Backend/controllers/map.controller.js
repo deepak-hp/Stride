e20561d1b06a28bc8334ddda88bc586e59a14865
Deepak  HP <deepakhp19@gmail.com>
2025-01-07 01:14:49 +0530

    Adding maps endpoints

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -15,3 +15,33 @@ module.exports.getCoordinates = async(req,res,next)=>{
         res.status(404).json({messge: "Coordinates not found", error: error.message})
     }
 }
+
+module.exports.getDistanceTime = async(req,res,next)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const {origin, destination} = req.query;
+    try{
+        const distanceAndTime = await mapService.getDistanceTime(origin, destination);
+        res.status(200).json(distanceAndTime)
+    }catch(error){
+        res.status(404).json({message: "Unable to find Distance and time", error: error.message})
+    }
+}
+
+module.exports.getAutoCompleteSuggetions = async (req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const {input} = req.query;
+
+    try {
+        const autocompleteSuggestions = await mapService.getAutoCompleteSuggetions(input);
+        res.status(200).json(autocompleteSuggestions)
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"})
+    }
+
+}
--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -1,15 +1,15 @@
 const axios = require("axios")
 
-module.exports.getAddressCoordinate = async (address) =>{
+module.exports.getAddressCoordinate = async (address) => {
     const apiKey = process.env.GOOGLE_MAPS_API
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}` 
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`
 
     try {
         const response = await axios.get(url)
-        if(response.data.status === 'OK'){
+        if (response.data.status === 'OK') {
             const location = response.data.results[0].geometry.location;
             console.log(location)
-            return{
+            return {
                 ltd: location.lat,
                 lng: location.lng
             }
@@ -19,4 +19,53 @@ module.exports.getAddressCoordinate = async (address) =>{
     } catch (error) {
         console.error(error.message)
     }
-} 
+}
+
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    if (!origin || !destination) {
+        throw new Error("Origin and distination are required")
+    }
+
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === "OK") {
+            if (response.data.rows[0].elements[0].status === "ZERO_RESULTS") {
+                throw new Error("No routes found");
+            }
+
+            return response.data.rows[0].elements[0]
+        } else {
+            throw new Error("Unable to fetch distance and time")
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports.getAutoCompleteSuggetions = async (input) => {
+    if (!input) {
+        throw new Error("query is required")
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        if(response.data.status === "OK"){
+            return response.data.predictions;
+        } else {
+            throw new Error("Unable to fetch suggestions")
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
